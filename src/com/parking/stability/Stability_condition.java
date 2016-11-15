2490a734f80df53416de38f338b45d76baa1d86c
paridhika <paridhika.kayal@gmail.com>
2016-11-15 16:35:11 -0500

    arrival rates updated

--- src/com/parking/stability/Stability_condition.java
+++ src/com/parking/stability/Stability_condition.java
@@ -25,15 +25,15 @@ public class Stability_condition {
 	private static void initialize_mqtt(){
 		mu1 = 0.0035; //get 
 		mu2 = 0.0015; //put
-		mu3 = 0.0017; //delete
+		mu3 = 0.0015; //delete
 		nu = 0.3;
 	}
 	
 	private static void initialize_xmpp(){
-		mu1 = 0.06; //get
-		mu2 = 0.05; //put
-		mu3 = 0.07; //delete
-		nu = 1;
+		mu1 = 0.015; //get
+		mu2 = 0.015; //put
+		mu3 = 0.015; //delete
+		nu = 0.3;
 	}
 	
 	private static void initialize_mqttws(){
@@ -43,8 +43,8 @@ public class Stability_condition {
 		nu = 1;
 	}
 	private static void server_utilization(double utilization){
-		for (double i = 0.01; i<(67/nu);i+=0.01){
-			for( double j=0.01;(i+j) < (67/nu); j+=0.01){
+		for (double i = 0.1; i<(67/nu);i+=0.01){
+			for( double j=0.1;(i+j) < (67/nu); j+=0.05){
 				double a = i*(mu1+mu3*nu);
 				double b = j*(mu2+mu3*nu);
 				if(a+b >= (utilization-0.0001) && a+b <= (utilization+0.0001)){
@@ -61,32 +61,32 @@ public class Stability_condition {
 		initialize_xmpp();
 		//initialize_mqttws();
 	//	server_utilization(0.2);
-		server_utilization(0.6);
-	//	server_utilization(0.8);
+	//	server_utilization(0.6);
+		server_utilization(0.8);
 	}
 	
 	 /* CoAP Utilization rates
-	 * 20% - get = 1.2000000000000004 put = 1.1000000000000003
-	 * 60% - get = 5.04999999999999 put = 2.549999999999999
-	 * 80% - get = 7.599999999999981 put = 2.9499999999999975
+	  * 20% - get = 1.2000000000000004 put = 1.1000000000000003
+	  * 60% - get = 5.04999999999999 put = 2.549999999999999
+	  * 80% - get = 7.599999999999981 put = 2.9499999999999975
 	 */
 	
 	 /* MQTT Utilization rates
-	 * 20% - get = 1.0 put = 97.5 
-	 * 60% - get = 98.5 put = 102.0
-	 * 80% - get = 189.0 put = 21
+	  * 20% - get = 1.0 put = 97.5 
+	  * 60% - get = 98.5 put = 102.0
+	  * 80% - get = 189.0 put = 21
 	 */
 	
-	 /* MQTT Utilization rates
-		 * 20% - get = 0.68 put = 0.93 
-		 * 60% - get = 98.5 put = 102.0
-		 * 80% - get = 189.0 put = 21
-	*/
+	 /* XMPP Utilization rates
+	  * 20% - get = 3.2 put = 7.05 
+	  * 60% - get = 13.27 put = 17.5
+	  * 80% - get = 19.98 put = 20.05
+	 */
 	
 	 /* MQTTWS Utilization rates
-	 * 20% - get = 1.0 put = 14.5
-	 * 60% - get = 3.0 put = 43.5
-	 * 80% - get = 6.0 put = 7.0
+	  * 20% - get = 1.0 put = 14.5
+	  * 60% - get = 3.0 put = 43.5
+	  * 80% - get = 6.0 put = 7.0
 	 */
 
 }
